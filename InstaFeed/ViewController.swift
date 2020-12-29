de8a20d33d81d347a4e4a7e525c00b2d0c7af600
Hemal Modi <hemalmodi3@gmail.com>
2020-12-29 21:46:30 +0400

    Initial Commit

---
+++ InstaFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaFeed
+//
+//  Created by Hemal on 29/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
